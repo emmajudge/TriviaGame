8efae1ed16ed71f9547985e2f999ce2c62d6e10a
emmajudge <emmalyn.judge@u.northwestern.edu>
2019-09-23 11:06:29 -0500

    fixed show possible answers and reset button, just add radio to force single resp

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,11 +22,8 @@ var inCorrectAnswers = 0;
 var unAnswers = 0;
 var index = 0;
 
-var timer=10;
+var timer = 10;
 var intervalid;
-
-var resetButton=document.createElement("button");resetButton.innerHTML="Play Again!"
-resetButton.addEventListener("click", playAgain());
 
 function showTrivia() {
 
@@ -34,7 +31,7 @@ function showTrivia() {
         $("#showPossibleAnswers").append(questionAnswers[index].question + "<br>");
         for (var i = 0; i < questionAnswers[index].answers.length; i++) {
             var p = $("<p>")
-            p.attr("data-rightAnswer",questionAnswers[index].rightAnswer)
+            p.attr("data-rightAnswer", questionAnswers[index].rightAnswer)
             p.addClass("answer")
             p.attr("data-possibleAnswer", questionAnswers[index].answers[i])
             p.html(questionAnswers[index].answers[i] + "<br><br>")
@@ -49,56 +46,74 @@ function showTrivia() {
     });
 
 
-    $(".answer").on("click", function(){
+    $(".answer").on("click", function () {
+        console.log(this)
 
         var getRightAnswer = $(this).attr("data-rightAnswer");
         var getPossibleAnswer = $(this).attr("data-possibleAnswer");
-        
-        if( getPossibleAnswer === getRightAnswer)
-          {
-              correctAnswers++
-          }  
-          else{
-              inCorrectAnswers++
-          }
+
+        if (getPossibleAnswer === getRightAnswer) {
+            correctAnswers++
+        }
+        else {
+            inCorrectAnswers++
+        }
     })
 
 }
-    showTrivia()
+
+showTrivia()
 
 
-    intervalid= setInterval(countDown,1000);
+intervalid = setInterval(countDown, 1000);
 
 // create countdown function to display time remaining or lock time and display results once time has has elapsed (display results function that is called here is defined below -- daisy chain style)
-function countDown()
-{
-    $("#timer").html("Timer: "+timer)
+function countDown() {
+    $("#timer").text("Timer: " + timer)
     timer--;
-    if(timer===0)
-    {
-       clearInterval(intervalid);
-       displayResults(); 
-    } 
-    
-}
-
-// define function that, once the time has elapsed, will 1) hide the timer, 2) hide the possible answers (so user can no longer select answers after time is up), and 3) show the user's overall results number of right/wrong responses and unanswered questions --> with # unanswered as a formula of total questions minus number correct and incorrect (so total right+wrong+unanswered equals the total number of questions)
-    function displayResults(){
-
-          $("#timer").hide();
-          $("#showPossibleAnswers").hide();
-          $("#results").show();
-
-          $("#results").append("correct answer:" + correctAnswers +"<br>")
-          $("#results").append("Incorrect answer:" + inCorrectAnswers +"<br>")
-
-          unAnswers=questionAnswers.length -correctAnswers -inCorrectAnswers
-          $("#results").append("Unanswer:" + unAnswers +"<br>")
-          $("#results").append(resetButton) 
+    if (timer === 0) {
+        clearInterval(intervalid);
+        displayResults();
     }
 
-function playAgain(){
-    $("#results").hide();
+}
+
+// when I added the reset button, it duplicated all of the possible answers on the 
+// show trivia screen and messed up the result counts.. do i need to define within
+// the displayResults function?
+
+// var resetButton=document.createElement("button");resetButton.innerHTML="Play Again!"
+// resetButton.addEventListener("click", playAgain());
+
+// define function that, once the time has elapsed, will 1) hide the timer, 2) hide the possible answers (so user can no longer select answers after time is up), and 3) show the user's overall results number of right/wrong responses and unanswered questions --> with # unanswered as a formula of total questions minus number correct and incorrect (so total right+wrong+unanswered equals the total number of questions)
+function displayResults() {
+
+    $("#timer").hide();
+    $("#showPossibleAnswers").hide();
+    $("#results").show();
+
+    $("#results").append("correct answer:" + correctAnswers + "<br>")
+    $("#results").append("Incorrect answer:" + inCorrectAnswers + "<br>")
+
+    unAnswers = questionAnswers.length - correctAnswers - inCorrectAnswers
+    $("#results").append("Unanswer:" + unAnswers + "<br>")
+    $("#results").append("<button onclick='playAgain()'>play again</button>")
+    //$("button).on("click", playAgain())
+}
+
+function playAgain() {
+
+    $("#results").empty().hide();
+    $("#timer").empty().show();
+    $("#showPossibleAnswers").empty().show();
+  
+    timer = 10
+    correctAnswers = 0;
+    inCorrectAnswers = 0;
+    unAnswers = 0;
+    index = 0;
+
+    intervalid = setInterval(countDown, 1000);
+
     showTrivia();
-    intervalid;
 }
